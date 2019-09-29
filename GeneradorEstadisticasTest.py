9af80a65b6e49d8133148c0737fbb5f095e75f0c
Catalina <cata.cordoba8@gmail.com>
2019-09-29 10:23:54 -0500

    Maximo con cadena dos numeros - Rojo

--- GeneradorEstadisticasTest.py
+++ GeneradorEstadisticasTest.py
@@ -22,3 +22,6 @@ class TestGeneradorEstadisticas(TestCase):
 
     def test_calcular_maximo_con_un_numero(self):
         self.assertEqual(GeneradorEstadisticas().calcular("3"), [1,3,3], "maximo con cadena un numero")
+
+    def test_calcular_maximo_con_dos_numero(self):
+        self.assertEqual(GeneradorEstadisticas().calcular("3,10"), [2,3,10], "maximo con cadena dos numeros")
