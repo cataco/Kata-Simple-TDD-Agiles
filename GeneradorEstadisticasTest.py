bd32eddc9ca45aee65e1397dcf4849671be9f6ad
Eduard <eduard.duarte@hotmail.com>
2019-09-29 10:02:36 -0500

    minimo con dos numeros - rojo

--- GeneradorEstadisticasTest.py
+++ GeneradorEstadisticasTest.py
@@ -10,8 +10,8 @@ class TestGeneradorEstadisticas(TestCase):
   #  def test_calcular_numero_elementos_un_numero(self):
   #      self.assertEqual(GeneradorEstadisticas().calcular("8"), [1], "numero elementos con 1 numero")
 
-    def test_calcular_numero_elementos_dos_numeros(self):
-        self.assertEqual(GeneradorEstadisticas().calcular("8,5"), [2], "numero elementos con 2 numeros")
+  #  def test_calcular_numero_elementos_dos_numeros(self):
+  #      self.assertEqual(GeneradorEstadisticas().calcular("8,5"), [2], "numero elementos con 2 numeros")
 
     def test_calcular_numero_elementos_n_numeros(self):
         self.assertEqual(GeneradorEstadisticas().calcular("8,5,1,4,5,3,8"), [7], "numero elementos con n numeros")
@@ -21,3 +21,6 @@ class TestGeneradorEstadisticas(TestCase):
 
     def test_calcular_minimo_con_un_numero(self):
         self.assertEqual(GeneradorEstadisticas().calcular("5"), [1,5], "minimo con cadena un numero")
+
+    def test_calcular_minimo_con_dos_numeros(self):
+        self.assertEqual(GeneradorEstadisticas().calcular("5,2"), [2,2], "minimo con cadena dos numeros")
