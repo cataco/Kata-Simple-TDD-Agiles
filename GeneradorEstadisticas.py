9ec6111790f475d93800c4232e4070e21e01f0a6
Eduard <eduard.duarte@hotmail.com>
2019-09-29 10:13:40 -0500

    minimo n numeros - verde

--- GeneradorEstadisticas.py
+++ GeneradorEstadisticas.py
@@ -2,8 +2,6 @@ class GeneradorEstadisticas:
     def calcular(self, cadena):
         if cadena == "":
             return [0]
-        elif "," not in cadena:
-            return [1,int(cadena)]
         else:
             numeros = cadena.split(",")
-            return [len(numeros),int(min(numeros[0],numeros[1]))]
+            return [len(numeros),int(min(numeros))]
