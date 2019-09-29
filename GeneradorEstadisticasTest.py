98bcd1bbed69df54852b75849f89167b39b656f1
Eduard <eduard.duarte@hotmail.com>
2019-09-29 10:43:57 -0500

    promedio dos numeros - Rojo

--- GeneradorEstadisticasTest.py
+++ GeneradorEstadisticasTest.py
@@ -23,3 +23,5 @@ class TestGeneradorEstadisticas(TestCase):
     def test_calcular_promedio_con_un_numero(self):
         self.assertEqual(GeneradorEstadisticas().calcular("7"), [1,7,7,7], "promedio con un numero")
 
+    def test_calcular_promedio_con_dos_numeros(self):
+        self.assertEqual(GeneradorEstadisticas().calcular("7,3"), [1,3,7,5], "promedio con dos numeros")
