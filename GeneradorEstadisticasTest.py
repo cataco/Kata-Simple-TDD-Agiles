dc0a3c33aeccf7b49dec34cb8db6f5ed6f9c2f9b
Catalina <cata.cordoba8@gmail.com>
2019-09-29 10:32:57 -0500

    Maximo con cadena n numeros - Verde

--- GeneradorEstadisticasTest.py
+++ GeneradorEstadisticasTest.py
@@ -5,19 +5,7 @@ from GeneradorEstadisticas import GeneradorEstadisticas
 
 class TestGeneradorEstadisticas(TestCase):
 
-    def test_calcular_minimo_y_numero_elementos_cadena_vacia(self):
-        self.assertEqual(GeneradorEstadisticas().calcular(""), [0], "minimo con cadena vacia")
-
-    #def test_calcular_minimo_con_un_numero(self):
-    #    self.assertEqual(GeneradorEstadisticas().calcular("5"), [1,5], "minimo con cadena un numero")
-
-    #def test_calcular_minimo_con_dos_numeros(self):
-    #    self.assertEqual(GeneradorEstadisticas().calcular("5,2"), [2,2], "minimo con cadena dos numeros")
-
-    #def test_calcular_minimo_con_n_numeros(self):
-    #    self.assertEqual(GeneradorEstadisticas().calcular("5,2,4,1"), [4,1], "minimo con cadena n numeros")
-
-    def test_calcular_maximo_elementos_cadena_vacia(self):
+    def test_calcular_minimo_maximo_elementos_cadena_vacia(self):
         self.assertEqual(GeneradorEstadisticas().calcular(""), [0], "maximo con cadena vacia")
 
     def test_calcular_maximo_con_un_numero(self):
