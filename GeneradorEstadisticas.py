5689776bc9488d7a46d2cb66791da2fd765286a0
Catalina <cata.cordoba8@gmail.com>
2019-09-29 10:21:06 -0500

    Maximo con cadena un numero - Verde

--- GeneradorEstadisticas.py
+++ GeneradorEstadisticas.py
@@ -2,6 +2,8 @@ class GeneradorEstadisticas:
     def calcular(self, cadena):
         if cadena == "":
             return [0]
+        elif "," not in cadena:
+            return [1, int(cadena), int(cadena)]
         else:
             numeros = cadena.split(",")
             return [len(numeros),int(min(numeros))]
